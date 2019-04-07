44ab076e052c9769b5d6d6e573ba9174ffe44daf
pedro <peaugust@hotmail.com>
2019-04-07 18:45:21 -0300

    Criando a página de criação de veículo

--- src/views/CreateVehicle.vue
+++ src/views/CreateVehicle.vue
@@ -1,11 +1,37 @@
 <template>
-
+  <v-container>
+    <v-content>
+      <v-layout>
+        <v-flex xs12>
+          <v-card>
+            <v-card-title>
+              <div>
+                <div class="headline">Cadastrar Veículo</div>
+                <span>Insira abaixo as informações para criar um veículo</span>
+              </div>
+            </v-card-title>
+            <v-card-text>
+              <FormNewVehicle>  </FormNewVehicle>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn class="primary">Salvar</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-content>
+  </v-container>
 </template>
 
 <script>
-  export default {
-    name: 'CreateVehicle'
+import FormNewVehicle from './../components/forms/FormNewVehicle'
+export default {
+  name: 'CreateVehicle',
+  components: {
+    FormNewVehicle
   }
+}
 </script>
 
 <style scoped>
