61e4ba42219896c1aca7670d6ab925787d66c3d9
pedro <peaugust@hotmail.com>
2019-04-07 19:16:04 -0300

    Refatorando arquivo

---
+++ src/views/Schedule.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-content>
+      <v-layout>
+        <v-flex xs12>
+          <v-card>
+            <v-card-title>
+              <div>
+                <div class="headline">Agendamentos</div>
+                <span>Insira abaixo as informações para criar um veículo</span>
+              </div>
+            </v-card-title>
+            <v-card-text>
+              <!--TODO: Inserir a Tabela de agendamentos-->
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-content>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Schedule'
+  }
+</script>
+
+<style scoped>
+
+</style>
