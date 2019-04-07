a212ba84bd286ddf782dcafcfe60623949538c11
pedro <peaugust@hotmail.com>
2019-04-07 19:14:55 -0300

    Inserindo v-model no formulário e fazendo ajustes no campo de data de nascimento

--- src/components/forms/FormNewUser.vue
+++ src/components/forms/FormNewUser.vue
@@ -2,10 +2,10 @@
   <v-container fluid grid-list-md>
     <v-layout row wrap>
       <v-flex xs12 md6 d-flex>
-        <v-text-field label="Nome"></v-text-field>
+        <v-text-field label="Nome" v-model="user.name"></v-text-field>
       </v-flex>
       <v-flex xs12 md6 d-flex>
-        <v-text-field label="Sobrenome"></v-text-field>
+        <v-text-field label="Sobrenome" v-model="user.surnname"></v-text-field>
       </v-flex>
       <v-flex xs12 md6 d-flex>
         <v-menu
@@ -21,15 +21,14 @@
         >
           <template v-slot:activator="{ on }">
             <v-text-field
-              v-model="date"
-              label="Birthday date"
-              prepend-icon="event"
+              v-model="user.birthdate"
+              label="Data de nascimento"
               readonly
-              v-on="on"></v-text-field>
+              v-on="on" style="width: 100%"></v-text-field>
           </template>
           <v-date-picker
             ref="picker"
-            v-model="date"
+            v-model="user.birthdate"
             :max="new Date().toISOString().substr(0, 10)"
             min="1950-01-01"
             @change="save"
@@ -38,18 +37,18 @@
         </v-menu>
       </v-flex>
       <v-flex xs12 md6 d-flex>
-        <v-text-field label="CPF"></v-text-field>
+        <v-text-field label="CPF" v-model="user.CPF"></v-text-field>
       </v-flex>
       <v-flex xs12 md6 d-flex>
-        <v-text-field label="Carteira de motorista"></v-text-field>
+        <v-text-field label="Carteira de motorista" v-model="user.license"></v-text-field>
       </v-flex>
       <v-flex xs12 md6 d-flex>
-        <v-text-field label="Endereço"></v-text-field>
+        <v-text-field label="Endereço" v-model="user.address"></v-text-field>
       </v-flex>
       <v-flex xs12 md6 d-flex>
-        <v-text-field label="E-mail"></v-text-field>
+        <v-text-field label="E-mail" v-model="user.email"></v-text-field>
       </v-flex> <v-flex xs12 md6 d-flex>
-        <v-text-field label="Celular"></v-text-field>
+        <v-text-field label="Celular" v-model="user.phoneNumber"></v-text-field>
       </v-flex>
     </v-layout>
   </v-container>
@@ -61,7 +60,17 @@ export default {
   data () {
     return {
       menu: false,
-      date: ''
+      date: '',
+      user: {
+        name: '',
+        surnname: '',
+        birthdate: '',
+        CPF: '',
+        license: '',
+        address: '',
+        email: '',
+        phoneNumber: '',
+      }
     }
   },
   watch: {
