605be7ea8887b9ba0665fb6d291136767e4ee887
pedro <peaugust@hotmail.com>
2019-04-08 00:03:03 -0300

    Inserindo mascaras e v-models

--- src/components/forms/FormNewVehicle.vue
+++ src/components/forms/FormNewVehicle.vue
@@ -2,24 +2,24 @@
   <v-container fluid grid-list-md>
     <v-layout row wrap>
       <v-flex xs12 md6 d-flex>
-        <v-text-field label="Placa"></v-text-field>
+        <v-text-field label="Placa" mask="AAA-####" v-model="vehicle.plate"></v-text-field>
       </v-flex>
       <v-flex xs12 md6 d-flex>
-        <v-text-field label="Nº do renavam"></v-text-field>
+        <v-text-field label="Nº do renavam" v-model="vehicle.renavam"></v-text-field>
       </v-flex>
       <v-flex xs12 md6 d-flex>
-        <v-text-field label="Ano de fabricação"></v-text-field>
+        <v-text-field label="Ano de fabricação" mask="####" v-model="vehicle.fabricationYear"></v-text-field>
       </v-flex>
       <v-flex xs12 md6 d-flex>
-        <v-text-field label="Ano de compra"></v-text-field>
+        <v-text-field label="Ano de compra" mask="####" v-model="vehicle.purchaseYear"></v-text-field>
       </v-flex>
       <v-flex xs12 md6 d-flex>
-        <v-text-field label="Carga máxima suportada"></v-text-field>
+        <v-text-field label="Carga máxima suportada" v-model="vehicle.maxLoad"></v-text-field>
       </v-flex>
       <v-flex xs12 md6 d-flex>
-        <v-text-field label="Nº de eixos"></v-text-field>
+        <v-text-field label="Nº de eixos" mask="##" v-model="vehicle.numAxles"></v-text-field>
       </v-flex> <v-flex xs12 md6 d-flex>
-        <v-text-field label="Comprimento total"></v-text-field>
+        <v-text-field label="Comprimento total (em metros)" v-model="vehicle.totalLength"></v-text-field>
       </v-flex>
     </v-layout>
   </v-container>
@@ -31,19 +31,17 @@ export default {
   data () {
     return {
       menu: false,
-      date: ''
+      vehicle: {
+        plate: '',
+        renavam: '',
+        fabricationYear: '',
+        purchaseYear: '',
+        numAxles: '',
+        maxLoad: '',
+        totalLength: ''
+      }
     }
   },
-  watch: {
-    menu (val) {
-      val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
-    }
-  },
-  methods: {
-    save (date) {
-      this.$refs.menu.save(date)
-    }
-  }
 }
 </script>
 
